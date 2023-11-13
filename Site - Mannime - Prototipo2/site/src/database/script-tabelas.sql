8d9e538789f0a0d3bfb51042c873a4bc261ba13d
ThiagoThmaz <thiago.conceicao@sptech.school>
2023-11-13 00:13:22 -0300

    'SegundoPrototipo'

---
+++ Site - Mannime - Prototipo2/site/src/database/script-tabelas.sql
@@ -0,0 +1,12 @@
+Create database Mannime;
+Use Mannime;
+
+Create table cadastro(
+idCadastro int primary key,
+nome varchar(45),
+email varchar(45),
+senha varchar(45),
+cpf char(11));
+
+select * from cadastro;
+
